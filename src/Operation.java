6997059350622e76a8b4ff3071f53c6a72ba56f1
nathaniel cobbinah <baby.hugo74@yahoo.com>
2020-09-16 16:50:42 -0700

    Update Operation.java
    added the statements to retrieve the current client to be removed from the queue

--- src/Operation.java
+++ src/Operation.java
@@ -20,11 +20,15 @@ class Operation {
     public void printEvent(int x , Node node , boolean isFull) {
         String outPutString = "";
         if (isFull && x > 0){
-            outPutString = x+"--->  (  +- client  )";
+            //adding my changes here
+            Node client = queue.removeHead();
+            outPutString = x+"--->  (  "+-" +  client.name  )";
         }
 
         if (x == 0){
-            outPutString = x+"--->  (  -- client  )";
+            //adding my changes here
+            Node client = queue.removeHead();
+            outPutString = x+"--->  (  "--" + client.name  )";
         }else if(x == 1){
             outPutString = x+" ---> (  ++ VIP"+Node.LAST_VIP_INDEX+1+"  )";
         }else if (x == 2 || x == 3){
